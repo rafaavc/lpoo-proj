921816dfa2c1d9c294e6cbd9307004e31223a20c
rafaavc <rafavc2508@gmail.com>
2020-04-16 17:40:23 +0100

    Hello world!

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Test hello world!");
+    }
+}
