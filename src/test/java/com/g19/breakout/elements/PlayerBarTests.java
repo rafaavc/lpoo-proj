07d67dffa47d0445d439ca088188b6ecd0e34181
Xavier-Pisco <x.pisco@gmail.com>
2020-04-22 17:35:48 +0100

    PlayerBar tests for constructor, moveRight and moveLeft

---
+++ src/test/java/com/g19/breakout/elements/PlayerBarTests.java
@@ -0,0 +1,44 @@
+package com.g19.breakout.elements;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerBarTests {
+
+    @Test
+    public void playerBarConstructor(){
+        PlayerBar playerBar = new PlayerBar(new Position(10, 5));
+
+        assertEquals(10, playerBar.getPosition().getDiscreteX());
+        assertEquals(5, playerBar.getPosition().getDiscreteY());
+    }
+
+    @Test
+    public void playerBarMovementRight(){
+        PlayerBar playerBar = new PlayerBar(new Position(10, 10));
+
+        Position positionMock = Mockito.mock(Position.class);
+        Mockito.when(positionMock.right()).thenReturn(new Position(11, 10));
+
+        playerBar.moveRight();
+        Position position = playerBar.getPosition();
+        assertEquals(11, position.getDiscreteX());
+        assertEquals(10, position.getDiscreteY());
+
+    }
+
+    @Test
+    public void playerBarMovementeLeft(){
+        PlayerBar playerBar = new PlayerBar(new Position(10, 10));
+
+        Position positionMock = Mockito.mock(Position.class);
+        Mockito.when(positionMock.right()).thenReturn(new Position(9, 10));
+
+        playerBar.moveLeft();
+        Position position = playerBar.getPosition();
+        assertEquals(9, position.getDiscreteX());
+        assertEquals(10, position.getDiscreteY());
+    }
+}
