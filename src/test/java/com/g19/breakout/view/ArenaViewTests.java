d2c3360e8f198d92d53eaf400cb0a78ebd264f58
rafaavc <rafavc2508@gmail.com>
2020-04-30 17:10:12 +0100

    ArenaViewTests done

--- src/test/java/com/g19/breakout/view/ArenaViewTests.java
+++ src/test/java/com/g19/breakout/view/ArenaViewTests.java
@@ -2,7 +2,10 @@ package com.g19.breakout.view;
 
 import com.g19.breakout.elements.Position;
 import com.g19.breakout.model.ArenaModel;
-import com.g19.breakout.view.factory.BasicViewFactory;
+import com.g19.breakout.model.BallModel;
+import com.g19.breakout.model.PlayerBarModel;
+import com.g19.breakout.model.TileModel;
+import com.g19.breakout.view.factory.ViewFactory;
 import com.g19.breakout.view.graphics.Graphics;
 import com.g19.breakout.view.graphics.LanternaAdapter;
 import org.junit.Before;
@@ -10,73 +13,60 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class ArenaViewTests {
-    private ArenaModel arena;
     private Graphics graphics;
     private ArenaView view;
+    private ViewFactory factory;
 
-    /*
-    * To test this class maybe remove view's
-    * initialization from constructor
-    * to a set method
-    * */
-
-    //@Before
-    public void setup() throws IOException {
-        arena = Mockito.mock(ArenaModel.class);
+    @Before
+    public void setup() {
         graphics = Mockito.mock(LanternaAdapter.class);
-        view = new ArenaView(arena, graphics, new BasicViewFactory());
+        factory = Mockito.mock(ViewFactory.class);
     }
 
-    /*@Test
-    public void testConstructor() throws IOException {
-        verify(arena, times(1)).getHeight();
-        verify(arena, times(1)).getWidth();
-        verify(graphics, times(1)).init(arena.getWidth(), arena.getHeight());
-    }*/
-
-    /*@Test
+    @Test
     public void testDraw() throws IOException {
-        /* I don't think this needs testing */
-        /*ArenaView view1 = Mockito.mock(ArenaView.class);
-        view.draw(view1, arena);
+        BallView ballViewMock = Mockito.mock(BallView.class);
+        PlayerBarView playerBarViewMock = Mockito.mock(PlayerBarView.class);
+        TilesView tilesViewMock = Mockito.mock(TilesView.class);
 
+        Mockito.when(factory.createBallView(any(BallModel.class), any(Graphics.class))).thenReturn(ballViewMock);
+        Mockito.when(factory.createPlayerBarView(any(PlayerBarModel.class), any(Graphics.class))).thenReturn(playerBarViewMock);
+        Mockito.when(factory.createTilesView(any(TileModel.class), any(Graphics.class))).thenReturn(tilesViewMock);
+
+
+        view = new ArenaView(new ArenaModel(100, 100), graphics, factory);
+
+        ArenaView view1 = Mockito.spy(view);
+        view1.draw();
+
+        verify(view1, times(1)).drawBackground(any(ArenaModel.class));
         verify(graphics, times(1)).startDrawing();
-        verify(view1, times(1)).drawBackground(arena);
-        verify(view1, times(1)).drawPlayerBar(arena.getPlayerBar());
-        verify(view1, times(1)).drawBall(arena.getBall());
-        verify(graphics, times(1)).stopDrawing();*/
-    //}
-
-/*
-    @Test
-    public void testDrawBall() {
-        BallModel ball = Mockito.mock(BallModel.class);
-
-        view.drawBall(ball);
-        verify(graphics, times(1)).drawElement(ball);
+        verify(graphics, times(1)).stopDrawing();
+        verify(tilesViewMock, times(1)).draw(any(List.class));
+        verify(playerBarViewMock, times(1)).draw(any(PlayerBarModel.class));
+        verify(ballViewMock, times(1)).draw(any(BallModel.class));
     }
 
     @Test
-    public void testDrawPlayerBar() {
-        PlayerBarModel playerBar = Mockito.mock(PlayerBarModel.class);
-
-        view.drawPlayerBar(playerBar);
-        verify(graphics, times(1)).drawElement(playerBar);
-    }*/
-
-    //@Test
     public void testDrawBackground() {
+        view = new ArenaView(new ArenaModel(100, 100), graphics, factory);
+
+        ArenaModel arena = Mockito.mock(ArenaModel.class);
         view.drawBackground(arena);
         verify(graphics, times(1)).drawRectangle(new Position(0, 0), new Position(arena.getWidth(), arena.getHeight()), ' ', view.getBGColor());
     }
 
-    //@Test
+    @Test
     public void testReadInput() throws IOException {
+        view = new ArenaView(new ArenaModel(100, 100), graphics, factory);
+
         view.readInput();
         verify(graphics, times(1)).readInput();
     }
