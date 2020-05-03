f457ccd9696d3701f44ea8cdd40e1eb890c6e0da
rafaavc <rafavc2508@gmail.com>
2020-05-03 14:17:43 +0100

    Converted moveBall and movePlayerBar to moveElement (ArenaController)

--- src/main/java/com/g19/breakout/controller/commands/CommandLeft.java
+++ src/main/java/com/g19/breakout/controller/commands/CommandLeft.java
@@ -10,7 +10,7 @@ public class CommandLeft implements Command {
     public boolean execute(ArenaController controller) {
         // maybe change to where the playerbar is moved based on velocity, the longer the key is pressed the faster it moves
         PlayerBarModel playerBar = controller.getArena().getPlayerBar();
-        controller.movePlayerBar(playerBar.getPosition().left());
+        controller.moveElement(playerBar.getPosition().left(), playerBar);
         return true;
     }
 }
