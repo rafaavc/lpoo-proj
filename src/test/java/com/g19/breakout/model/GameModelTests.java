6f0fdefc82287cdfb27f7d9fa2314210c710f59f
Xavier-Pisco <x.pisco@gmail.com>
2020-05-18 11:37:02 +0100

    ModelFactory Tests

--- src/test/java/com/g19/breakout/model/GameModelTests.java
+++ src/test/java/com/g19/breakout/model/GameModelTests.java
@@ -4,14 +4,14 @@ import com.g19.breakout.elements.Dimensions;
 import com.g19.breakout.elements.Position;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class GameModelTests {
 
     @Test
     public void isInsideGameTest(){
         GameModel gameModel = new GameModel(new Dimensions(100, 50));
+        assertEquals(new Dimensions(100, 50), gameModel.getDimensions());
         assertFalse(gameModel.isInsideGame(new Position(100, 30), new Dimensions(2, 1)));
         assertTrue(gameModel.isInsideGame(new Position(10, 10), new Dimensions(10, 10)));
     }
--- src/test/java/com/g19/breakout/model/TileModelTests.java
+++ src/test/java/com/g19/breakout/model/TileModelTests.java
@@ -3,18 +3,31 @@ package com.g19.breakout.model;
 import com.g19.breakout.elements.Dimensions;
 import com.g19.breakout.elements.Position;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class TileModelTests {
 
     @Test
-    public void testHit(){
+    public void hitTest(){
         TileModel tile = new TileModel(new Position(10, 10), new Dimensions(1, 1), 2);
 
         assertFalse(tile.hit());
         assertEquals(1, tile.getLife());
         assertTrue(tile.hit());
         assertEquals(0, tile.getLife());
+    }
+
+    @Test
+    public void createTileArrayTest(){
+        ArenaModel arena = Mockito.mock(ArenaModel.class);
+        Mockito.when(arena.getWidth()).thenReturn(50);
+
+        List<TileModel> tiles = TileModel.createTileArray(5, 5, arena);
+        assertEquals(25, tiles.size());
+        assertEquals(new Position(5, 1), tiles.get(0).getPosition());
     }
 }
