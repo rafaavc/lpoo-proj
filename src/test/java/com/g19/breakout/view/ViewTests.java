aba917b7670306bd3743f6fdbde4478d1937816b
Xavier-Pisco <x.pisco@gmail.com>
2020-05-29 15:30:24 +0100

    LeaderboardView tests

--- src/test/java/com/g19/breakout/view/ViewTests.java
+++ src/test/java/com/g19/breakout/view/ViewTests.java
@@ -5,13 +5,16 @@ import com.g19.breakout.elements.Position;
 import com.g19.breakout.model.BackgroundModel;
 import com.g19.breakout.view.graphics.Graphics;
 import com.g19.breakout.view.graphics.LanternaAdapter;
+import com.sun.tools.javac.util.Pair;
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.Positive;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -19,11 +22,11 @@ import static org.mockito.ArgumentMatchers.eq;
 public class ViewTests {
 
     @Provide
-    Arbitrary<Integer> Between0And1000() {
-        return Arbitraries.integers().filter(n -> n > 0 && n < 1000);
+    Arbitrary<Integer> Between0And500() {
+        return Arbitraries.integers().filter(n -> n > 0 && n < 500);
     }
      @Property
-    public void BackgroundViewTests(@ForAll("Between0And1000") int n){
+    public void BackgroundViewTests(@ForAll("Between0And500") int n){
          BackgroundModel model = Mockito.mock(BackgroundModel.class);
          Graphics graphics = Mockito.mock(LanternaAdapter.class);
          Dimensions dimensions = new Dimensions(100, 120);
@@ -41,5 +44,23 @@ public class ViewTests {
 
          Mockito.verify(graphics, Mockito.times(1)).drawRectangle(new Position(0, 0), dimensions, ' ', "#000000");
          Mockito.verify(graphics, Mockito.times(n)).drawString(any(Position.class), any(String.class), any(String.class), any(String.class));
+     }
+
+     @Test
+    public void LeaderboardViewTests(){
+        Comparator<Pair<String, Integer>> cmp = (p1, p2) -> p2.snd - p1.snd;
+         PriorityQueue<Pair<String, Integer>> pq = new PriorityQueue<>(cmp);
+         pq.add(new Pair<>("NoOne", 10));
+         pq.add(new Pair<>("Another", 100));
+
+         Dimensions dimensions = new Dimensions(100, 120);
+         Graphics graphics = Mockito.mock(LanternaAdapter.class);
+
+         LeaderboardView leaderboardView = new LeaderboardView(graphics, dimensions, pq, "#000000");
+
+        Mockito.doNothing().when(graphics).drawString(any(Position.class), any(String.class), any(String.class), any(String.class));
+        leaderboardView.drawSelf();
+
+        Mockito.verify(graphics, Mockito.times(3*pq.size())).drawString(any(Position.class), any(String.class), any(String.class), any(String.class));
      }
 }
